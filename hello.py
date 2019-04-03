ddabed404c1fca297265c20a5cd160dfb4f860f3
sa77irak <sa77irak@gmail.com>
2019-04-03 11:24:17 +0900

    feat: edit hello.py
    switched _ to i in for loop to use internal variable.
    I add conditional statement to print 'hello world' when i is even

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 #hello world 출력!
 #hello world를 5번 출력하시오
-for _ in range(1,1+5):
-    print("Hello world!")
+#hello world를 내부 변수 i가 짝수일 때만 출력하시오.(i는 1부터 11까지 진행됨.)
+for i in range(1,10+1):
+    if i%2==0:
+        print("Hello world!")
